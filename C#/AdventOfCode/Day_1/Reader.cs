73529099c38169f5efdb1cbe3a6c7396c48de1e6
Victor <vielm18@student.sdu.dk>
2022-12-12 22:37:07 +0100

    Adding first 5 days

---
+++ C#/AdventOfCode/Day_1/Reader.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+
+namespace AdventOfCode.Day_1
+{
+    internal class Reader
+    {
+        private readonly string file = "C:\\Users\\Victor\\Documents\\C#\\AdventOfCode\\Day_1\\Calories.txt";
+
+        public ArrayList GetElfsFromFile()
+        {
+
+            ArrayList elfs = new ArrayList();
+
+            if (File.Exists(file))
+            {
+                Console.WriteLine("The File Exists");
+
+                Elf elf = new Elf("Elf " + "0");
+
+                // Read the file present in the path
+                using (StreamReader sr = new StreamReader(file))
+                {
+
+                    // Iterating the file
+                    while (sr.Peek() >= 0)
+                    {
+
+                        string line = sr.ReadLine();
+
+                        if (line.Equals("")) {
+                            elfs.Add(elf);
+
+                            elf = new Elf("Elf " + elfs.Count);
+
+                        } else
+                        {
+                            elf.addCalories(line);
+                        }
+
+
+                       
+                    }
+                }
+
+            }
+
+            return elfs;
+        }
+    }
+}
