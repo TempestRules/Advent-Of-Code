68b4dc633585c032f046f0cc9c85f390955ae1a6
Victor <43045051+TempestRules@users.noreply.github.com>
2024-12-02 18:13:32 +0100

    Day1
    And fix GitIgnore

---
+++ 2023/Day6/Program.cs
@@ -0,0 +1,69 @@
+﻿int[] raceTimes = { 47, 70, 75, 66 };
+int[] recordDistances = { 282, 1079, 1147, 1062 };
+
+long result = CalculateWaysToBeatRecord(raceTimes, recordDistances);
+
+Console.WriteLine("Assignment 1 Result: " + result);
+
+long[] raceTimes2 = { 47707566 };
+long[] recordDistances2 = { 282107911471062 };
+
+long result2 = CalculateWaysToBeatRecordLong(raceTimes2, recordDistances2);
+
+Console.WriteLine("Assignment 2 Result: " + result2);
+
+long CalculateWaysToBeatRecord(int[] raceTimes, int[] recordDistances)
+{
+    long totalWays = 1;
+
+    for (int i = 0; i < raceTimes.Length; i++)
+    {
+        int maxHoldTime = raceTimes[i] - 1; // Maximum time to hold the button
+
+        int recordDistance = recordDistances[i];
+        long waysToBeatRecord = 0;
+
+        for (int holdTime = 0; holdTime <= maxHoldTime; holdTime++)
+        {
+            int remainingTime = raceTimes[i] - holdTime - 1; // Remaining time after holding the button
+            int totalDistance = (holdTime + 1) * remainingTime;
+
+            if (totalDistance > recordDistance)
+            {
+                waysToBeatRecord++;
+            }
+        }
+
+        totalWays *= waysToBeatRecord;
+    }
+
+    return totalWays;
+}
+
+long CalculateWaysToBeatRecordLong(long[] raceTimes, long[] recordDistances)
+{
+    long totalWays = 1;
+
+    for (long i = 0; i < raceTimes.Length; i++)
+    {
+        long maxHoldTime = raceTimes[i] - 1; // Maximum time to hold the button
+
+        long recordDistance = recordDistances[i];
+        long waysToBeatRecord = 0;
+
+        for (long holdTime = 0; holdTime <= maxHoldTime; holdTime++)
+        {
+            long remainingTime = raceTimes[i] - holdTime - 1; // Remaining time after holding the button
+            long totalDistance = (holdTime + 1) * remainingTime;
+
+            if (totalDistance > recordDistance)
+            {
+                waysToBeatRecord++;
+            }
+        }
+
+        totalWays *= waysToBeatRecord;
+    }
+
+    return totalWays;
+}
---
+++ 2024/AdventOfCode/Day1/Program.cs
@@ -0,0 +1,45 @@
+﻿using AdventOfCodeUtil;
+
+var input = TextFileReaderService.ReadTextFile("Input.txt");
+
+// Assignment 1
+var startTime = DateTime.UtcNow;
+
+var leftList = new List<int>();
+var rightList = new List<int>();
+
+foreach (var line in input)
+{
+    var lists = line.Split("   ");
+
+    leftList.Add(int.Parse(lists[0]));
+    rightList.Add(int.Parse(lists[1]));
+}
+
+leftList.Sort();
+rightList.Sort();
+
+var totalDistance = leftList
+    .Zip(rightList, (left, right) => Math.Abs(right - left))
+    .Sum();
+
+var endTime = DateTime.UtcNow;
+
+Console.WriteLine($"The total distance is: {totalDistance}");
+Console.WriteLine($"Total time: {endTime - startTime}");
+
+// Assignment 2
+startTime = DateTime.UtcNow;
+
+var similarityScore = 0;
+foreach (var locationId in leftList)
+{
+    var count = rightList.Count(x => x == locationId);
+
+    similarityScore += locationId * count;
+}
+
+endTime = DateTime.UtcNow;
+
+Console.WriteLine($"The similarity score is: {similarityScore}");
+Console.WriteLine($"Total time: {endTime - startTime}");
